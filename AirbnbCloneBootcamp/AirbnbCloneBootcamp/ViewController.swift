bc92652f8ae0c901e3f2729ad5b6406237aa3c0a
EduLp99 <edulp99@gmail.com>
2024-08-05 22:04:29 -0300

    Adicionando projeto  configurado para viewCode

---
+++ AirbnbCloneBootcamp/AirbnbCloneBootcamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirbnbCloneBootcamp
+//
+//  Created by Eduardo Felipe das Neves Abido on 05/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
