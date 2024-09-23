e10ce53d985b4eb7119a9fe20962ea64bb5c9d77
EduLp99 <edulp99@gmail.com>
2024-09-23 20:22:41 -0300

    implementando firebase login

--- AirbnbCloneBootcamp/AirbnbCloneBootcamp/Login/LoginViewController.swift
+++ AirbnbCloneBootcamp/AirbnbCloneBootcamp/Login/LoginViewController.swift
@@ -7,39 +7,71 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-  var screen: LoginScreen?
-
-  override func viewWillAppear(_ animated: Bool) {
-    navigationController?.isNavigationBarHidden = true
-  }
-
-  override func loadView() {
-    screen = LoginScreen()
-    view = screen
-  }
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    screen?.delegate = self
-  }
-
+    
+    var screen: LoginScreen?
+    var viewModel: LoginViewModel = LoginViewModel()
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func loadView() {
+        screen = LoginScreen()
+        view = screen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        screen?.delegate = self
+        viewModel.delegate = self
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okButton)
+        present(alertController, animated: true)
+    }
+    
 }
 
 extension LoginViewController: LoginScreenProtocol {
-  func tappedSignIn() {
-    navigationController?.pushViewController(MainTabBarController(), animated: true)
-  }
-
-  func tappedCreateAccount() {
-    print(#function)
-  }
-
-  func tappedGoogle() {
-    print(#function)
-  }
-
-  func tappedFacebook() {
-    print(#function)
-  }
+    func tappedSignIn() {
+        
+        guard let email: String = screen?.emailTextField.text,
+              let password: String = screen?.passwordTextField.text,
+              !email.isEmpty,
+              !password.isEmpty else {
+            showAlert(title: "Atenção!", message: "Por favor, preencha todos os campos")
+            return
+        }
+        
+        viewModel.fetchLogin(email: email, password: password)
+    }
+    
+    func tappedCreateAccount() {
+        print(#function)
+    }
+    
+    func tappedGoogle() {
+        print(#function)
+    }
+    
+    func tappedFacebook() {
+        print(#function)
+    }
 }
+
+extension LoginViewController: LoginViewModelProtocol {
+    
+    func successLogin() {
+        navigationController?.pushViewController(MainTabBarController(), animated: true)
+        print("Login efetuado com sucesso!")
+    }
+    
+    func errorLogin(title: String, message: String) {
+        showAlert(title: title, message: message)
+    }
+}
+
--- AirbnbCloneBootcamp/AirbnbCloneBootcamp/Login/ViewModel/LoginViewModel.swift
+++ AirbnbCloneBootcamp/AirbnbCloneBootcamp/Login/ViewModel/LoginViewModel.swift
@@ -5,8 +5,29 @@
 //  Created by Eduardo Felipe das Neves Abido on 19/09/24.
 //
 import Foundation
+import FirebaseAuth
+
+
+protocol LoginViewModelProtocol: AnyObject {
+    func successLogin()
+    func errorLogin(title: String, message: String)
+}
 
 class LoginViewModel {
-
-  
+    
+    weak var delegate: LoginViewModelProtocol?
+    
+    func fetchLogin(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let self else { return }
+            
+            guard error == nil else {
+                delegate?.errorLogin(title: "Atenção!", message: error?.localizedDescription ?? "")
+                return
+            }
+            
+            delegate?.successLogin()
+        }
+    }
+    
 }
